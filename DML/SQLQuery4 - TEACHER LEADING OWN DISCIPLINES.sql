5db2994755d5ddb82c0f5d11035aeb842c0648ba
sandCat <Sand@DESKTOP-K3SU1F7>
2026-02-20 17:40:57 +0300

    The teacher leading own disciplines DONE

--- DML/SQLQuery4 - TEACHER LEADING OWN DISCIPLINES.sql
+++ DML/SQLQuery4 - TEACHER LEADING OWN DISCIPLINES.sql
@@ -9,5 +9,11 @@ SELECT
 	,COUNT(tdr.discipline)	AS		N' оличество дисциплин которые ведЄт преподаватель'
 FROM Teachers
 JOIN TeachersDisciplinesRelation AS tdr ON teacher_id = teacher 
-GROUP BY Teachers.last_name
+GROUP BY Teachers.teacher_id , Teachers.last_name, Teachers.first_name, Teachers.middle_name
+ORDER BY Teachers.last_name DESC
+--ORDER BY Ц упор€дочить (отсортировать) по;
+--ASC Ц (сокращение от ascending Ц восход€щий) пор€док то есть сортировка от меньшего к большему (0, 1, 2, 3 .. )
+--в случае сортировки слов Ц по алфавиту (ј -> я);
+--DESC - (сокращение от descending Ц нисход€щий) пор€док то есть сортировка от большего к меньшему (100, 99, 98 Е). 
+--—лова в этом случае сортируютс€ в пор€дке обратном алфавитному (я -> ј).
 ;
