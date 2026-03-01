cc8a479abda16396b6f0946a481c0797c4ef0151
sandCat <Sand@DESKTOP-K3SU1F7>
2026-03-01 22:36:36 +0300

    Lines of code for functiuon have been changed

--- StoredProcedures/SQLQuery1-sp Insert Schedule.sql
+++ StoredProcedures/SQLQuery1-sp Insert Schedule.sql
@@ -39,16 +39,18 @@ BEGIN
 		--SET @lesson_number = @lesson_number + 1;
 		--SET @time = DATEADD(MINUTE, 95, @time);
 		EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
-				--	PRINT(FORMATMESSAGE(N'%i   %s    %s    %s', @lesson_number, CAST(@date AS VARCHAR(24)), DATENAME(WEEKDAY,@date), CAST(@time AS VARCHAR(24))));
+		--	PRINT(FORMATMESSAGE(N'%i   %s    %s    %s', @lesson_number, CAST(@date AS VARCHAR(24)), DATENAME(WEEKDAY,@date), CAST(@time AS VARCHAR(24))));
 		--IF NOT EXISTS(SELECT lesson_id FROM Schedule WHERE [date] = @date AND [time] = @time AND [group] = @group)
 		--	INSERT INTO Schedule 
 		--	VALUES (@group, @discipline, @teacher, @date, @time, IIF(@date < GETDATE(), 1, 0));
 		--SET @lesson_number = @lesson_number + 1;
 		--SET @time = DATEADD(MINUTE, 95, @time);
 		EXEC sp_InsertLesson @group, @discipline, @teacher, @date, @time OUTPUT, @lesson_number OUTPUT;
-		DECLARE @day	AS TINYINT = DATEPART(WEEKDAY, @date);
+		DECLARE @day	AS TINYINT = dbo.GetNextLearningDay(@date, @time);
+		--DECLARE @day	AS TINYINT = DATEPART(WEEKDAY, @date);
 		--PRINT(@day);
-		SET @date	=	DATEADD(DAY, IIF(@day = 5, 3, 2), @date);
+		SET @date = dbo.GetNexLearningDate(@date, @time);
+		--SET @date	=	DATEADD(DAY, IIF(@day = 5, 3, 2), @date);
 	END
 END
 PRINT(@time);
