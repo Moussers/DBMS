5cf9b48d33047eda072acc3d0d7dd31adfd832b6
sandCat <Sand@DESKTOP-K3SU1F7>
2026-03-01 18:46:42 +0300

    Create function for definition on next data DONE

--- Function/SQLQuery1-Get Next Learning Day.sql
+++ Function/SQLQuery1-Get Next Learning Day.sql
@@ -6,10 +6,19 @@ CREATE OR ALTER FUNCTION GetNextLearningDay(@date AS DATE, @time AS TIME) RETURN
 	
 AS
 BEGIN
-	DECLARE @current AS DATE;
-	SELECT TOP 1 @current = [date]
+	DECLARE @current_lesson AS DATE;
+	SELECT TOP 1 @current_lesson = [date]
 	FROM Schedule
 	WHERE [date] > @date OR [date] = @date AND [time] > @time
 	ORDER BY [date], [time];
-	RETURN (@current);
+	RETURN (@current_lesson);
+	--Если ближайшая занятие на неделе не найдено
+	IF(@current_lesson IS NULL)
+	BEGIN
+		SELECT TOP 1 @current_lesson = [date]
+		FROM Schedule
+		WHERE [date] > DATEADD(WEEK, 1, @date) OR [date] = DATEADD(WEEK, 1, @date) AND [time] > @time
+		ORDER BY [date], [time]
+	END
+	RETURN (@current_lesson);
 END;
