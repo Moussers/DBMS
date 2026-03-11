dce020607f81dd10899c2f6357cfebf29b7a59d3
Sand <sand999.com>
2026-03-11 17:04:37 +0300

    Migration from Windows 10 to Windows 11

--- Function/SQLQuery0-Check.sql
+++ Function/SQLQuery0-Check.sql
@@ -8,5 +8,5 @@ EXEC sp_SelectScheduleFor N'PV_521'
 
 PRINT dbo.GetNextLearningDay(N'PV_521', N'2026-03-05');
 PRINT dbo.GetNextLearningDate(N'SPU_411', N'2026-03-07');
-PRINT dbo.GetSummertimeSadness(N'2025')
+PRINT dbo.GetSummertimeSadness(2025)
 PRINT dbo.GetEasterDate(1999);
